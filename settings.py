21f084ec0101ac7ede73753d51daa706de70af60
Natalia Tsed <106387902+NataliaTsed@users.noreply.github.com>
2023-07-29 20:29:51 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,29 @@
+""""API библиотека к сайту http://34.141.58.52:8000/docs#/"""
+"""UI часть к сайту http://34.141.58.52:8080/#/"""
+
+
+class RegisterData:
+    VALID_EMAIL = 'natali1793@mail.ru'
+    VALID_PASSWORD = 'Natali1793'
+    EXPIRED_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODU4NzI3NjIsImlhdCI6MTY4NTgwMDc2Miwic3ViIjoxMzc2fQ.ONuix4YCy0QduEs3iDTd5I0xEEbjGBsZJnJSvYRZkHA'
+
+
+class RequestURL:
+    BASE_URL = 'http://34.141.58.52:8000/'
+
+
+class MyPets:
+    NAME_CAT = 'Sara'
+    NAME_HAMSTER = 'Jeremy'
+    TYPE_CAT = 'cat'
+    GENDER_CAT = 'Female'
+    TYPE_HAMSTER = 'hamster'
+    AGE_CAT = '5'
+    AGE_HAMSTER = '1'
+    GENDER_HAMSTER = 'Male'
+    MESSAGE = "The best pet in the World!"
+
+
+class OtherPets:
+    NAME_PET = 'Bob'
+    TYPE_PET = 'cat'
